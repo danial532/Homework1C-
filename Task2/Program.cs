68e83f574d93a8090db283e849e351cbda45c538
Danial <tretjakov.danial@yandex.ru>
2022-07-07 00:37:30 +0300

    Выполнено второе задание

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+        Console.Clear();
+        Console.WriteLine("Введите число А ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число B");   
+        int B = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число C");
+        int C = Convert.ToInt32(Console.ReadLine());
+
+        {
+            if (A > B && A > C)
+            {
+             Console.WriteLine($"Максимальное число = {A}");
+            }
+                if (B > A && B > C)
+                {
+                 Console.WriteLine($"Максимальное число = {B}");
+                }
+                    if (C > A && C > B)
+                    {
+                     Console.WriteLine($"Максимальное число = {C}");
+                    }
+            
+        }
+    }
+}

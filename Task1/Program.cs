bd2b6ffc19f1927298845aae145cac8874f717b9
Danial <tretjakov.danial@yandex.ru>
2022-07-06 22:51:09 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+        Console.Clear();
+         Console.WriteLine("Введите число А ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число B");
+        int B = Convert.ToInt32(Console.ReadLine());
+        
+        {
+        if (A < B) 
+        Console.WriteLine ($" {B} > {A}");
+        else
+            if (A>B) Console.WriteLine ($" {A} > {B}");
+            else 
+            Console.WriteLine ($"числа равны, {A} = {B}");
+        }
+    }
+}

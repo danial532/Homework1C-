87ac6f464211fb84824f80947d88cc4adbc13e46
Danial <tretjakov.danial@yandex.ru>
2022-07-09 17:02:11 +0300

    Выполнено третье задание

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+        Console.Clear();
+        Console.WriteLine("Введите число: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        
+        {
+            if (A % 2 == 0)
+            {
+             Console.WriteLine($"Число {A} является четным =) ");
+            }
+            else 
+            {
+            Console.WriteLine ($"Число {A} является нечетным =( ");
+            }
+        }     
+    }
+}
